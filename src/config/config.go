fb231935203edda22cc7e5b9a10e00def9ec05e6
Lukas <99747166+LukasLow@users.noreply.github.com>
2026-05-20 23:00:14 +0200

    change: Achive name to +Achrive

--- src/config/config.go
+++ src/config/config.go
@@ -3,7 +3,7 @@ package config
 const (
 	InputDir = "/documents/900-eingang"
 	TrashDir = "/documents/900-Trash"
-	BaseArch = "/documents/Archiv"
+	BaseArch = "/documents/+Archiv"
 	YamlPath = "/documents/korespondenten.yml"
 	Port     = ":4000"
 )
